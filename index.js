1e5c5c7e21e86cd0447c805cc9e41f3510c396e5
shani <shanipatel5008@gmail.com>
2026-05-14 22:15:38 +0530

    fix api route to lowercase

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 }))
 app.use(express.json());
 
-app.use('/api/Projects', require('./routes/Projects'));
+app.use('/api/projects', require('./routes/Projects'));
 app.use('/api/contact', require('./routes/contact'));
 
 const PORT = process.env.PORT || 8000;
